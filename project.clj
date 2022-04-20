351dabff2734f1d7e969def2e40434fa644be671
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2022-04-20 22:00:03 +0100

    Fix build

--- project.clj
+++ project.clj
@@ -2,19 +2,18 @@
   :description "A small, closeable test server to catch http calls you don't mock"
   :github/private? false
   :url "https://github.com/piotr-yuxuan/ephemeral-server"
-  :license {:name "GNU General Public License v3.0 or later"
-            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"
-            :distribution :repo
-            :comments "See also GPL_ADDITION.org"}
+  :license {:name "European Union Public License 1.2 or later"
+            :url "https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12"
+            :distribution :repo}
   :scm {:name "git"
         :url "https://github.com/piotr-yuxuan/ephemeral-server"}
   :pom-addition [:developers [:developer
                               [:name "胡雨軒 Петр"]
                               [:url "https://github.com/piotr-yuxuan"]]]
   :global-vars {*warn-on-reflection* true}
-  :plugins [[lein-tools-deps "0.4.5"]]
-  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:project]}
+  :dependencies [[metosin/reitit-ring "0.5.12"]
+                 [aleph/aleph "0.4.7-alpha7"]
+                 [org.clojure/clojure "1.10.0"]]
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
                                     :username :env/WALTER_CLOJARS_USERNAME
