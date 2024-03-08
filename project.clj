2b6fb122ed2c5198852cd654508f1198704b43ca
Walter CI <piotr-yuxuan@users.noreply.github.com>
2024-03-08 18:32:37 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
   :global-vars {*warn-on-reflection* true}
   :dependencies [[metosin/reitit-ring "0.7.0-alpha7"]
                  [aleph/aleph "0.7.1"]
-                 [org.clojure/clojure "1.12.0-alpha8"]]
+                 [org.clojure/clojure "1.12.0-alpha9"]]
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
                                     :username :env/WALTER_CLOJARS_USERNAME
