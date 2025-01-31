485243540411bcd4bc0effd3145ef8d65d8f09c5
Walter CI <piotr-yuxuan@users.noreply.github.com>
2025-01-31 18:41:34 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                               [:name "胡雨軒 Петр"]
                               [:url "https://github.com/piotr-yuxuan"]]]
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[metosin/reitit-ring "0.7.2"]
+  :dependencies [[metosin/reitit-ring "0.8.0-alpha1"]
                  [aleph/aleph "0.8.2"]
                  [org.clojure/clojure "1.12.0"]]
   :deploy-repositories [["clojars" {:sign-releases false
